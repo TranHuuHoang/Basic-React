3054fbf7101362b44863a8b656b52ed8a82cc9bc
Hoang <thhoang1111999@gmail.com>
2020-05-25 18:02:18 +0800

    Assignment 2'

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -18,7 +19,6 @@ class Main extends Component { // A Container Component
   constructor(props) {
     super(props);
     this.state = {
-      selectedDish: null,
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
@@ -62,6 +62,7 @@ class Main extends Component { // A Container Component
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />} />
               <Redirect to="/home" />
         </Switch>
         <Footer />
